f1c15b717bf81b3d6abf071270d0a9d219d7b002
A Rotem <aaron.rotem@gmail.com>
2020-09-21 00:11:50 -0400

    added error, spinner, and loading to the FE

--- static/main.js
+++ static/main.js
@@ -10,6 +10,9 @@
     "$http",
     "$timeout",
     function($scope, $log, $http, $timeout) {
+      $scope.submitButtonText = 'Submit'
+      $scope.loading = false;
+      $scope.lineerror = false;
       function getOdds(jobID) {
         var timeout = "";
     
@@ -20,11 +23,19 @@
               $log.log(data,status);
             } else if (status === 200){
               $log.log(data);
+              $scope.loading = false;
+              $scope.submitButtonText = "Submit";
               $scope.dataResults = data
               $timeout.cancel(timeout);
               return false;
             }
             timeout = $timeout(poller, 2000)
+          }).
+          error(function(error){
+            $log.log(error);
+            $scope.loading = false;
+            $scope.submitButtonText = "Submit"
+            $scope.lineerror = true;
           });
         };
         poller();
@@ -32,11 +43,13 @@
 
 
       $scope.getResults = function() {
-        $log.log("world");
-
         $http.post('/start').success(function(results){
             $log.log(results);
             getOdds(results)
+            $scope.lineerror = false;
+            $scope.dataResults = null;
+            $scope.loading = true;
+            $scope.submitButtonText = 'Loading...'
         }).
         error(function(error){
             $log.log(error);
