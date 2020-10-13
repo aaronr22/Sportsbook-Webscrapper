50e18cacc468704b42af54346de41d4f6c9566a0
A Rotem <aaron.rotem@gmail.com>
2020-10-13 13:37:22 -0600

    commented out some FE code

--- static/main.js
+++ static/main.js
@@ -7,55 +7,55 @@
     "$http",
     "$timeout",
     function($scope, $log, $http, $timeout) {
-      $scope.submitButtonText = 'Submit'
-      $scope.loading = false;
-      $scope.lineerror = false;
-      function getOdds(jobID) {
-        var timeout = "";
+      // $scope.submitButtonText = 'Submit'
+      // $scope.loading = false;
+      // $scope.lineerror = false;
+      // function getOdds(jobID) {
+      //   var timeout = "";
     
-        var poller = function() {
-          $http.get('/results/'+jobID).
-          success(function(data, status, headers, config) {
-            if(status === 202){
-              $log.log(data,status);
-            } else if (status === 200){
-              $log.log(data);
-              $scope.loading = false;
-              $scope.submitButtonText = "Submit";
-              $scope.dataResults = data
-              $timeout.cancel(timeout);
-              return false;
-            }
-            timeout = $timeout(poller, 2000)
-          }).
-          error(function(error){
-            $log.log(error);
-            $scope.loading = false;
-            $scope.submitButtonText = "Submit"
-            $scope.lineerror = true;
-          });
-        };
-        poller();
-      }
+      //   var poller = function() {
+      //     $http.get('/results/'+jobID).
+      //     success(function(data, status, headers, config) {
+      //       if(status === 202){
+      //         $log.log(data,status);
+      //       } else if (status === 200){
+      //         $log.log(data);
+      //         $scope.loading = false;
+      //         $scope.submitButtonText = "Submit";
+      //         $scope.dataResults = data
+      //         $timeout.cancel(timeout);
+      //         return false;
+      //       }
+      //       timeout = $timeout(poller, 2000)
+      //     }).
+      //     error(function(error){
+      //       $log.log(error);
+      //       $scope.loading = false;
+      //       $scope.submitButtonText = "Submit"
+      //       $scope.lineerror = true;
+      //     });
+      //   };
+      //   poller();
+      // }
 
-      //Gets called first when the button is clicked
-      $scope.getResults = function() {
-        var userInput = $scope.radio;
-        if(userInput != 'NFL' && userInput != 'CFB'){
-          userInput = 'NFL'
-        }
-        $http.post('/start', {'radio': userInput}).success(function(results){
-            $log.log(results);
-            getOdds(results)
-            $scope.lineerror = false;
-            $scope.dataResults = null;
-            $scope.loading = true;
-            $scope.submitButtonText = 'Loading...'
-        }).
-        error(function(error){
-            $log.log(error);
-        });
-      };
+      // //Gets called first when the button is clicked
+      // $scope.getResults = function() {
+      //   var userInput = $scope.radio;
+      //   if(userInput != 'NFL' && userInput != 'CFB'){
+      //     userInput = 'NFL'
+      //   }
+      //   $http.post('/start', {'radio': userInput}).success(function(results){
+      //       $log.log(results);
+      //       getOdds(results)
+      //       $scope.lineerror = false;
+      //       $scope.dataResults = null;
+      //       $scope.loading = true;
+      //       $scope.submitButtonText = 'Loading...'
+      //   }).
+      //   error(function(error){
+      //       $log.log(error);
+      //   });
+      // };
 
       $scope.queryResults = function() {
         $log.log("1")
